8ee5b48dbf57a6bc90ffc61ace3222e9fdfe5398
PRAVEEN KUMAR GAUTAM <pkg2027@gmail.com>
2025-11-01 12:24:33 +0530

    fixed errors in data.sh

--- data.sh
+++ data.sh
@@ -1,17 +1,14 @@
 !#/bin/bash
-# Saving output
-"""
 date
 echo ""
 hostnamectl
 echo ""
 lscpu
 echo ""
-os-release
+cat etc/os-release
 echo ""
 who
 echo ""
 uname -asrm
 echo ""
-ss- antpl
-""" >> data.txt
+ss -antpl
